b3dbbd4d1e1baeb3de60c7b45dc253443c8ce530
eikefrota <eikefrotaa@hotmail.com>
2025-04-16 14:46:18 -0300

    Finalizando o consumo da API ViaCep

--- html-css/zero85games/js/app.js
+++ html-css/zero85games/js/app.js
@@ -1,17 +1,30 @@
 
-document.getElementById("btnBuscarCep").addEventListener("click", (e) => {
-    e.preventDefault(); 
-    const cep = document.getElementById("cep").value;
-    buscarCep(cep);
+document.getElementById("btnBuscarCEP").addEventListener('click', () => {
+    const cep = document.getElementById('cep').value;
+    buscarCEP(cep);
 });
 
-function buscarCep(cep){
-    const url = `https://viacep.com.br/ws/${cep}/json/`;
+function buscarCEP(cep) {
+    // const link = 'https://viacep.com.br/ws/01001000/json/';
+    const link = 'https://viacep.com.br/ws/' + cep + '/json/';
 
-    fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-    })
+    // Envia o CEP informado para a API do ViaCEP (WebService externo)
+    fetch(link)
+    .then(
+        // Se tiver sucesso, retorna a resposta com os valores
+        response => response.json()
+        // Converte os valores para o formato JSON
+    )
+    .then(
+        // Preenche os campos do endereço
+        dados => preencherDados(dados)
+    )
+}
 
+function preencherDados(dados) {    
+    document.getElementById('logradouro').value = dados.logradouro;
+    document.getElementById('bairro').value = dados.bairro;
+    document.getElementById('localidade').value = dados.localidade;
+    document.getElementById('uf').value = dados.uf;
+    document.getElementById('regiao').value = dados.regiao;
 }
